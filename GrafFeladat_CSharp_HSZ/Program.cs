40cc91078af5ecc8e3eccd41b942f1e2b4f3afa9
garru <garruswasab@gmail.com>
2020-01-26 23:34:25 +0100

    Add project files.

---
+++ GrafFeladat_CSharp_HSZ/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrafFeladat_CSharp_HSZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var graf = new Graf(6);
+
+            graf.Hozzaad(0, 1);
+            graf.Hozzaad(1, 2);
+            graf.Hozzaad(0, 2);
+            graf.Hozzaad(2, 3);
+            graf.Hozzaad(3, 4);
+            graf.Hozzaad(4, 5);
+            graf.Hozzaad(2, 4);
+
+            Console.WriteLine(graf);
+            graf.SzelessegiBejar(4);
+            Console.ReadLine();
+        }
+    }
+}
